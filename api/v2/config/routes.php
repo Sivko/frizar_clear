90dd4c8d1e792f0dcde823bc1ad974ab566cb8d6
Sivakov Vladislav <svv@salesap.ru>
2024-10-21 09:42:43 +0300

    chown

--- api/v2/config/routes.php
+++ api/v2/config/routes.php
@@ -32,7 +32,7 @@ return function (App $app) {
         $response = $response
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             // ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Origin', 'https://vs113.ru')
+            ->withHeader('Access-Control-Allow-Origin', 'https://frizar.ru')
             // ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
